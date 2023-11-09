00687ea4fbd04329ab96ab1a0cf97083a851f1c1
YogeshM0re <ymore@chuckads.com>
2023-11-09 18:09:02 +0530

    Added:Validation to image.

--- CardwithGifts/src/routes/+page.svelte
+++ CardwithGifts/src/routes/+page.svelte
@@ -9,100 +9,80 @@
   let inputPassword = "JohnDoe";
 
   function handleLogin() {
-    user.username = inputUsername;
-    user.password = inputPassword;
+      user.username = inputUsername;
+      user.password = inputPassword;
 
-    if (user.username === "JohnDoe" && user.password === "JohnDoe") {
-      loggedIn.set(true);
-      username.set(user.username);
-      goto("/dashboard");
-    } else {
-      console.log("Login failed. Please check your credentials.");
-    }
+      if (user.username === "JohnDoe" && user.password === "JohnDoe") {
+          loggedIn.set(true);
+          username.set(user.username);
+          goto("/dashboard");
+      } else {
+          console.log("Login failed. Please check your credentials.");
+      }
   }
 </script>
 
 <div class="container mx-auto p-2 flex justify-center items-center h-screen">
-  <div
-    class="login-container"
-    style="background: linear-gradient(#F5DDDA, #D5EAF5); padding: 2rem; box-shadow: 0 4px 14px 0 rgba(0, 0, 0, 0.1); border: 0.1px solid grey; border-radius: 0.375rem;"
-  >
-    <h1 class="text-2xl mb-4 text-center text-grey">Welcome Back!</h1>
+  <div class="login-container"
+      style="background: linear-gradient(#F5DDDA, #D5EAF5); padding: 2rem; box-shadow: 0 4px 14px 0 rgba(0, 0, 0, 0.1); border: 0.1px solid grey; border-radius: 0.375rem;">
+      <h1 class="text-2xl mb-4 text-center text-grey">Welcome Back!</h1>
 
-    <form
-      class="flex flex-col space-y-6 items-center"
-      on:submit|preventDefault={handleLogin}
-    >
-      <label class="flex flex-col">
-        <span class="text-gray-700 mb-2 text-lg">Username</span>
-        <input
-          bind:value={inputUsername}
-          class="input w-64 rounded-lg py-3 px-4 bg-white"
-          type="text"
-          placeholder="Enter your username"
-        />
-      </label>
+      <form class="flex flex-col space-y-6 items-center" on:submit|preventDefault={handleLogin}>
+          <label class="flex flex-col">
+              <span class="text-gray-700 mb-2 text-lg">Username</span>
+              <input bind:value={inputUsername} class="input w-64 rounded-lg py-3 px-4 bg-white" type="text"
+                  placeholder="Enter your username" />
+          </label>
 
-      <label class="flex flex-col">
-        <span class="text-gray-700 mb-2 text-lg">Password</span>
-        <input
-          bind:value={inputPassword}
-          class="input w-64 rounded-lg py-3 px-4 bg-white"
-          type="password"
-          placeholder="Enter your password"
-        />
-      </label>
+          <label class="flex flex-col">
+              <span class="text-gray-700 mb-2 text-lg">Password</span>
+              <input bind:value={inputPassword} class="input w-64 rounded-lg py-3 px-4 bg-white" type="password"
+                  placeholder="Enter your password" />
+          </label>
 
-      <button
-        type="submit"
-        class="btn bg-purple-400 text-white py-2 rounded-lg hover:bg-purple-800"
-      >
-        Login
-      </button>
-    </form>
+          <button type="submit" class="btn bg-purple-400 text-white py-2 rounded-lg hover:bg-purple-800">
+              Login
+          </button>
+      </form>
 
-    <div class="flex flex-col mt-6 space-y-4">
-      <a href="/" class="text-black text-center hover:underline"
-        >Forgot Password?</a
-      >
+      <div class="flex flex-col mt-6 space-y-4">
+          <a href="/" class="text-black text-center hover:underline">Forgot Password?</a>
 
-      <p class="text-center mt-6">
-        Don't have an account?
-        <a
-          href="/registration"
-          style="text-decoration: none; color: #1a73e8; font-weight: bold; transition: color 0.3s ease;"
-          onmouseover="this.style.color='#ea4335'"
-          onmouseout="this.style.color='#1a73e8'"
-        >
-          Create one
-        </a>
-      </p>
-    </div>
+          <p class="text-center mt-6">
+              Don't have an account?
+              <a href="/registration"
+                  style="text-decoration: none; color: #1a73e8; font-weight: bold; transition: color 0.3s ease;"
+                  onmouseover="this.style.color='#ea4335'" onmouseout="this.style.color='#1a73e8'">
+                  Create one
+              </a>
+          </p>
+      </div>
   </div>
 </div>
 
 <style>
   .btn.bg-purple-400 {
-    background: #845ec2;
-    color: white;
-    padding: 8px 16px;
-    border: none;
-    border-radius: 8px;
-    transition: background 0.3s;
+      background: #845ec2;
+      color: white;
+      padding: 8px 16px;
+      border: none;
+      border-radius: 8px;
+      transition: background 0.3s;
   }
 
   .btn.bg-purple-400:hover {
-    background: linear-gradient(45deg, #faacb9, #fcb1d0, #d7bffc, #bbb3f9);
-    color: black;
-    animation: colorTransition 1s ease infinite;
+      background: linear-gradient(45deg, #faacb9, #fcb1d0, #d7bffc, #bbb3f9);
+      color: black;
+      animation: colorTransition 1s ease infinite;
   }
 
   @keyframes colorTransition {
-    0% {
-      filter: hue-rotate(0deg);
-    }
-    100% {
-      filter: hue-rotate(360deg);
-    }
+      0% {
+          filter: hue-rotate(0deg);
+      }
+
+      100% {
+          filter: hue-rotate(360deg);
+      }
   }
 </style>
--- CardwithGifts/src/routes/registration/+page.svelte
+++ CardwithGifts/src/routes/registration/+page.svelte
@@ -14,163 +14,113 @@
   let password = "";
 
   function handleSubmit() {
-    const formData = {
-      companyName,
-      phone,
-      phoneType,
-      email,
-      streetAddress,
-      address2,
-      city,
-      state,
-      zipCode,
-      username,
-      password,
-    };
+      const formData = {
+          companyName,
+          phone,
+          phoneType,
+          email,
+          streetAddress,
+          address2,
+          city,
+          state,
+          zipCode,
+          username,
+          password,
+      };
 
-    console.log("Form Data:", formData);
+      console.log("Form Data:", formData);
   }
 </script>
 
 <div class="container mx-auto p-4">
   <div
-    class="registration-form bg-gradient-to-br from-purple-300 to-purple-300 p-8 shadow-lg rounded-md border-2 border-purple-400"
-  >
-    <h1 class="text-4xl  mb-6 text-center text-grey">
-      User Registration
-    </h1>
+      class="registration-form bg-gradient-to-br from-purple-300 to-purple-300 p-8 shadow-lg rounded-md border-2 border-purple-400">
+      <h1 class="text-4xl  mb-6 text-center text-grey">
+          User Registration
+      </h1>
 
-    <form
-      class="flex flex-col space-y-6"
-      style="width: 400px; margin: 0 auto;"
-      on:submit|preventDefault={handleSubmit}
-    >
-      <label class="flex flex-col">
-        <span class="text-gray-700 mb-1">Company Name</span>
-        <input
-          class="input rounded-md py-2 px-3"
-          type="text"
-          placeholder="Enter your company name"
-          bind:value={companyName}
-        />
-      </label>
+      <form class="flex flex-col space-y-6" style="width: 400px; margin: 0 auto;"
+          on:submit|preventDefault={handleSubmit}>
+          <label class="flex flex-col">
+              <span class="text-gray-700 mb-1">Company Name</span>
+              <input class="input rounded-md py-2 px-3" type="text" placeholder="Enter your company name"
+                  bind:value={companyName} />
+          </label>
 
-      <label class="flex flex-col">
-        <span class="text-gray-700 mb-1">Phone</span>
-        <input
-          class="input rounded-md py-2 px-3"
-          type="text"
-          placeholder="Enter your phone number"
-          bind:value={phone}
-        />
-      </label>
+          <label class="flex flex-col">
+              <span class="text-gray-700 mb-1">Phone</span>
+              <input class="input rounded-md py-2 px-3" type="text" placeholder="Enter your phone number"
+                  bind:value={phone} />
+          </label>
 
-      <label class="flex flex-col">
-        <span class="text-gray-700 mb-1">Phone Type</span>
-        <select class="input rounded-md py-2 px-3" bind:value={phoneType}>
-          <option value="mobile">Mobile</option>
-          <option value="landline">Landline</option>
-          <option value="other">Other</option>
-        </select>
-      </label>
+          <label class="flex flex-col">
+              <span class="text-gray-700 mb-1">Phone Type</span>
+              <select class="input rounded-md py-2 px-3" bind:value={phoneType}>
+                  <option value="mobile">Mobile</option>
+                  <option value="landline">Landline</option>
+                  <option value="other">Other</option>
+              </select>
+          </label>
 
-      <label class="flex flex-col">
-        <span class="text-gray-700 mb-1">Email Address</span>
-        <input
-          class="input rounded-md py-2 px-3"
-          type="email"
-          placeholder="Enter your email"
-          bind:value={email}
-        />
-      </label>
-      <label class="flex flex-col">
-        <span class="text-gray-700 mb-1">Street Address</span>
-        <input
-          class="input rounded-md py-2 px-3"
-          type="text"
-          placeholder="Enter your street address"
-          bind:value={streetAddress}
-        />
-      </label>
+          <label class="flex flex-col">
+              <span class="text-gray-700 mb-1">Email Address</span>
+              <input class="input rounded-md py-2 px-3" type="email" placeholder="Enter your email"
+                  bind:value={email} />
+          </label>
+          <label class="flex flex-col">
+              <span class="text-gray-700 mb-1">Street Address</span>
+              <input class="input rounded-md py-2 px-3" type="text" placeholder="Enter your street address"
+                  bind:value={streetAddress} />
+          </label>
 
-      <label class="flex flex-col">
-        <span class="text-gray-700 mb-1">Suite/Street Address 2</span>
-        <input
-          class="input rounded-md py-2 px-3"
-          type="text"
-          placeholder="Enter your suite/street address 2"
-          bind:value={address2}
-        />
-      </label>
+          <label class="flex flex-col">
+              <span class="text-gray-700 mb-1">Suite/Street Address 2</span>
+              <input class="input rounded-md py-2 px-3" type="text" placeholder="Enter your suite/street address 2"
+                  bind:value={address2} />
+          </label>
 
-      <label class="flex flex-col">
-        <span class="text-gray-700 mb-1">City</span>
-        <input
-          class="input rounded-md py-2 px-3"
-          type="text"
-          placeholder="Enter your city"
-          bind:value={city}
-        />
-      </label>
+          <label class="flex flex-col">
+              <span class="text-gray-700 mb-1">City</span>
+              <input class="input rounded-md py-2 px-3" type="text" placeholder="Enter your city" bind:value={city} />
+          </label>
 
-      <label class="flex flex-col">
-        <span class="text-gray-700 mb-1">State</span>
-        <input
-          class="input rounded-md py-2 px-3"
-          type="text"
-          placeholder="Enter your state"
-          bind:value={state}
-        />
-      </label>
+          <label class="flex flex-col">
+              <span class="text-gray-700 mb-1">State</span>
+              <input class="input rounded-md py-2 px-3" type="text" placeholder="Enter your state"
+                  bind:value={state} />
+          </label>
 
-      <label class="flex flex-col">
-        <span class="text-gray-700 mb-1">Zip code</span>
-        <input
-          class="input rounded-md py-2 px-3"
-          type="text"
-          placeholder="Enter your zip code"
-          bind:value={zipCode}
-        />
-      </label>
+          <label class="flex flex-col">
+              <span class="text-gray-700 mb-1">Zip code</span>
+              <input class="input rounded-md py-2 px-3" type="text" placeholder="Enter your zip code"
+                  bind:value={zipCode} />
+          </label>
 
-      <label class="flex flex-col">
-        <span class="text-gray-700 mb-1">Username</span>
-        <input
-          class="input rounded-md py-2 px-3"
-          type="text"
-          placeholder="Enter your username"
-          bind:value={username}
-        />
-      </label>
+          <label class="flex flex-col">
+              <span class="text-gray-700 mb-1">Username</span>
+              <input class="input rounded-md py-2 px-3" type="text" placeholder="Enter your username"
+                  bind:value={username} />
+          </label>
 
-      <label class="flex flex-col">
-        <span class="text-gray-700 mb-1">Password</span>
-        <input
-          class="input rounded-md py-2 px-3"
-          type="password"
-          placeholder="Enter your password"
-          bind:value={password}
-        />
-      </label>
+          <label class="flex flex-col">
+              <span class="text-gray-700 mb-1">Password</span>
+              <input class="input rounded-md py-2 px-3" type="password" placeholder="Enter your password"
+                  bind:value={password} />
+          </label>
 
-      <button
-        type="submit"
-        class="btn skeleton-gold-nouveau-bg skeleton-gold-nouveau-text py-3 rounded-lg hover:bg-purple-800 transform transition duration-300 hover:scale-105"
-      >
-        Register
-      </button>
-      <p class="text-center mt-6">
-        Already have an account?
-        <a
-          href="/"
-          style="text-decoration: none; color: #1a73e8; font-weight: bold; transition: color 0.3s ease;"
-          onmouseover="this.style.color='#ea4335'"
-          onmouseout="this.style.color='#1a73e8'"
-        >
-          Login here
-        </a>
-      </p>
-    </form>
+          <button type="submit"
+              class="btn skeleton-gold-nouveau-bg skeleton-gold-nouveau-text py-3 rounded-lg hover:bg-purple-800 transform transition duration-300 hover:scale-105">
+              Register
+          </button>
+          <p class="text-center mt-6">
+              Already have an account?
+              <a href="/"
+                  style="text-decoration: none; color: #1a73e8; font-weight: bold; transition: color 0.3s ease;"
+                  onmouseover="this.style.color='#ea4335'" onmouseout="this.style.color='#1a73e8'">
+                  Login here
+              </a>
+          </p>
+      </form>
   </div>
 </div>
 
@@ -179,34 +129,34 @@
   input[type="email"],
   input[type="password"],
   select {
-    border: 1px solid #ccc;
-    transition: border-color 0.3s;
-    border: 1px solid #ccc;
-  transition: border-color 0.3s;
-  color: black; 
-  background-color: white;
+      border: 1px solid #ccc;
+      transition: border-color 0.3s;
+      border: 1px solid #ccc;
+      transition: border-color 0.3s;
+      color: black;
+      background-color: white;
   }
 
   input[type="text"]:focus,
   input[type="email"]:focus,
   input[type="password"]:focus,
   select:focus {
-    border-color: blue;
+      border-color: blue;
   }
 
   .registration-form {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
 
   }
 
   .bg-gradient-to-br {
-    background: linear-gradient( #D5EAF5 , #F5DDDA);
+      background: linear-gradient(#D5EAF5, #F5DDDA);
   }
 
   .btn:hover {
-    background: linear-gradient(#eab9fd , #F5DDDA);
+      background: linear-gradient(#eab9fd, #F5DDDA);
   }
 </style>
